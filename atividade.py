7be60135a0a998d19ebd992f407778ae024a9a8c
luiz-Guilhrme777 <luiz_g_garcia@estudante.sesisenai.org.br>
2024-08-26 15:34:51 -0300

    primeiro commit

---
+++ atividade.py
@@ -0,0 +1,9 @@
+soma = 0
+for i in range(1, 51):
+    soma += i
+    print("Soma:", soma)
+
+#linha 1: a soma zero define o valor como zero.
+#linha 2 ele define uma condição, enquanto ela nção for cumprida o código continuara repitindo
+#linha 3: a cada repetição o valor soma que esta definido como zero ira receber 1
+#linha 4: no final de cada repetição ele imprime, o valor do digito 
